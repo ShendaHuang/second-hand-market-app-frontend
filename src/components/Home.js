8cf6ea5998b5219e01e87eafed7dd40a0b7653b6
wangziyannb <wangziyannb@gmail.com>
2022-06-27 08:21:00 -0400

    Login & Logout & Register

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home(props) {
+    return (
+        <div>
+            playground for next components:)
+        </div>
+
+    );
+}
+
+export default Home;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'http://localhost:8080';
